f391a7b3888a93d29a4830a2b8810e31f0d1717c
Leandro Lisboa Penz <lpenz@lpenz.org>
2024-05-06 18:58:52 +0100

    test_hfv_regex: improve coverage

--- tests/test_hfv_regex.py
+++ tests/test_hfv_regex.py
@@ -38,10 +38,82 @@ class TestBasic(unittest.TestCase, Base):
         control = self.controlLines(2, ["abc", "def", "ghi"], 0)
         self.assertEqual(control.get_lines_style(), [[("", "abc")], [("", "def")]])
 
-    def test_matchall_oneline(self) -> None:
+    def test_matchall_lines(self) -> None:
         control = self.controlLines(2, ["abc", "def", "ghi"], 0)
         control.use_regex(re.compile(b"abc"))
         self.assertEqual(
             control.get_lines_style(),
             [[("class:match", "abc")], [("", "def")]],
+        )
+
+    def test_matchall_line2(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"def"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "abc")], [("class:match", "def")]],
+        )
+
+    def test_matchall_line3(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"ghi"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "abc")], [("", "def")]],
+        )
+
+    def test_matchall_line_mid(self) -> None:
+        control = self.controlLines(3, ["abc", "def", "ghi", "jkl"], 0)
+        control.use_regex(re.compile(b"def"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "abc")], [("class:match", "def")], [("", "ghi")]],
+        )
+
+    def test_matchall_char0(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"a"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("class:match", "a"), ("", "bc")], [("", "def")]],
+        )
+
+    def test_matchall_char1(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"b"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "a"), ("class:match", "b"), ("", "c")], [("", "def")]],
+        )
+
+    def test_matchall_char2(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"c"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "ab"), ("class:match", "c")], [("", "def")]],
+        )
+
+    def test_matchall_char3(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"d"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "abc")], [("class:match", "d"), ("", "ef")]],
+        )
+
+    def test_matchall_char4(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"f"))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "abc")], [("", "de"), ("class:match", "f")]],
+        )
+
+    def test_matchall_linecross(self) -> None:
+        control = self.controlLines(2, ["abc", "def", "ghi"], 0)
+        control.use_regex(re.compile(b"c.?d", re.DOTALL))
+        self.assertEqual(
+            control.get_lines_style(),
+            [[("", "ab"), ("class:match", "c")], [("class:match", "d"), ("", "ef")]],
         )
